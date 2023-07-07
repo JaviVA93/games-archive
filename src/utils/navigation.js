a2684a4b9ae5a68f44986bc451f3e467fd18cb77
Javi Villar <javiroll93@gmail.com>
2023-07-08 00:34:30 +0200

    forgotten changes

---
+++ src/utils/navigation.js
@@ -0,0 +1,33 @@
+const checkIsNavigationSupported = () => {
+    return Boolean(document.startViewTransition)
+}
+
+const fetchPage = async (url) => {
+    const response = await fetch(url);
+    const text = await response.text();
+    const [, data] = text.match(/<body>([\s\S]*)<\/body>/i);
+    console.log(data)
+    return data
+}
+
+export const startViewTransition = () => {
+    if (!checkIsNavigationSupported())
+        return
+
+    window.navigation.addEventListener("navigate", (event) => {
+        const toUrl = new URL(event.destination.url);
+        if (location.origin !== toUrl.origin) return;
+
+        event.intercept({
+            async handler() {
+                const data = await fetchPage(toUrl.pathname)
+
+                document.startViewTransition(() => {
+                    document.body.innerHTML = data
+
+                    document.documentElement.scrollTop = 0
+                });
+            },
+        });
+    });
+}
